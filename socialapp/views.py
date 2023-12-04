eb985851703631d15eb3de6023d155fe356ad6cc
sruthips <sruthips1820@gmail.com>
2023-12-04 18:19:50 +0530

    secnd commit

--- socialapp/views.py
+++ socialapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.generic import View,FormView,CreateView
+from socialapp.forms import RegistrationForm
+from django.urls import reverse
 
-# Create your views here.
+class SignUpView(CreateView):
+    template_name='register.html'
+    form_class=RegistrationForm
+
+    def get_success_url(self):
+        return reverse('signup')
+
+   
